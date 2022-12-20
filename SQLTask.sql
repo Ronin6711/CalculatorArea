416c75a54c48c88ad584e9a336496245e94f2be8
Ronin <arturdemchenko05@gmail.com>
2022-12-21 00:03:26 +0300

    File type change

--- SQLTask.sql
+++ SQLTask.sql
@@ -1,4 +1,4 @@
-Select p."NameProduct", c."NameCategories"
-From Products p
+select p."NameProduct", c."NameCategories"
+from Products p
 left join ProductCategories pc on p.ID = pc.ProductId
 left join Categories c on pc.CategoryId = c.ID
